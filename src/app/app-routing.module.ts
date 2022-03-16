0b0d04986b3b0f8f1863bc8aa81be8b0c3d8c077
Otvos-Adam-Balazs <79146953+Otvos-Adam-Balazs@users.noreply.github.com>
2022-03-16 22:42:10 +0100

    Bead

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListCarsComponent } from './list-cars/list-cars.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path: '',component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'list', component: ListCarsComponent},
+  {path: 'register' , component:RegisterComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
